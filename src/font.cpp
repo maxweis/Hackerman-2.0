e1678913a364adb0f1ad0993dbc4f73d3ba6bcc3
Max Weis <maxrobweis@gmail.com>
2018-04-24 18:04:15 -0500

    return text box rectangle from drawing functions

--- src/font.cpp
+++ src/font.cpp
@@ -1,45 +1,52 @@
 #include "font.h"
 
-void Font::DrawTopLeftAlign(std::string text, float x, float y, ofColor color) {
+ofRectangle Font::DrawTopLeftAlign(std::string text, float x, float y, ofColor color) {
   ofRectangle text_box = getStringBoundingBox(text, x, y);
+  text_box.y += text_box.height;
 
   ofSetColor(color);
   drawString(text, x, y + text_box.height);
+
+  return text_box;
 }
 
 //default border = 0
 //draw text at top left of bound with optional border spacing
-void Font::DrawTopLeftAlign(std::string text, ofRectangle bound, ofColor color, int border) {
-  DrawTopLeftAlign(text, bound.x + border, bound.y + border, color);
+ofRectangle Font::DrawTopLeftAlign(std::string text, ofRectangle bound, ofColor color, int border) {
+  return DrawTopLeftAlign(text, bound.x + border, bound.y + border, color);
 }
 
-void Font::DrawCenterAlign(std::string text, float x, float y, ofColor color) {
+ofRectangle Font::DrawCenterAlign(std::string text, float x, float y, ofColor color) {
   ofRectangle text_box = getStringBoundingBox(text, x, y);
 
   ofSetColor(color);
   drawString(text, x - (text_box.width / 2), y + (text_box.height / 2));
+
+  return text_box;
 }
 
 //draw text in center of bound
-void Font::DrawCenterAlign(std::string text, ofRectangle bound, ofColor color) {
+ofRectangle Font::DrawCenterAlign(std::string text, ofRectangle bound, ofColor color) {
   float bound_center_x = bound.x + (bound.width / 2);
   float bound_center_y = bound.y + (bound.height / 2);
 
-  DrawCenterAlign(text, bound_center_x, bound_center_y, color);
+  return DrawCenterAlign(text, bound_center_x, bound_center_y, color);
 }
 
 //draw text centered horizontally
 //y_ratio determines y from ratio of height
-void Font::DrawCenterAlignX(std::string text, ofRectangle bound, float y_ratio, ofColor color) {
+ofRectangle Font::DrawCenterAlignX(std::string text, ofRectangle bound, float y_ratio, ofColor color) {
   float bound_center_x = bound.x + (bound.width / 2);
   float bound_y = bound.y + (y_ratio * bound.height);
 
-  DrawCenterAlign(text, bound_center_x, bound_y, color);
+  return DrawCenterAlign(text, bound_center_x, bound_y, color);
 }
 
-void Font::DrawBottomLeftAlign(std::string text, float x, float y, ofColor color) {
+ofRectangle Font::DrawBottomLeftAlign(std::string text, float x, float y, ofColor color) {
   ofRectangle text_box = getStringBoundingBox(text, x, y);
 
   ofSetColor(color);
   drawString(text, x, y);
+
+  return text_box;
 }
--- src/font.h
+++ src/font.h
@@ -2,16 +2,17 @@
 
 #include "ofMain.h"
 #include "render.h"
+#include <iostream>
 
 class Font : public ofTrueTypeFont {
  public:
   //inherit constructors of parent class
   using ofTrueTypeFont::ofTrueTypeFont;
 
-  void DrawTopLeftAlign(std::string text, float x, float y, ofColor color);
-  void DrawTopLeftAlign(std::string text, ofRectangle rect, ofColor color, int border = 0);
-  void DrawCenterAlign(std::string text, float x, float y, ofColor color);
-  void DrawCenterAlign(std::string text, ofRectangle rect, ofColor color);
-  void DrawCenterAlignX(std::string text, ofRectangle bound, float y_ratio, ofColor color);
-  void DrawBottomLeftAlign(std::string text, float x, float y, ofColor color);
+  ofRectangle DrawTopLeftAlign(std::string text, float x, float y, ofColor color);
+  ofRectangle DrawTopLeftAlign(std::string text, ofRectangle rect, ofColor color, int border = 0);
+  ofRectangle DrawCenterAlign(std::string text, float x, float y, ofColor color);
+  ofRectangle DrawCenterAlign(std::string text, ofRectangle rect, ofColor color);
+  ofRectangle DrawCenterAlignX(std::string text, ofRectangle bound, float y_ratio, ofColor color);
+  ofRectangle DrawBottomLeftAlign(std::string text, float x, float y, ofColor color);
 };
