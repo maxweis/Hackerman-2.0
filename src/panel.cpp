5f96ef35445d2fac4320f84cab27c5530d8c4f98
Max Weis <maxrobweis@gmail.com>
2018-05-02 23:38:13 -0500

    add more helpful 'help' message

--- src/panel.cpp
+++ src/panel.cpp
@@ -181,7 +181,7 @@ void Hackerman::ProcessCommand() {
     console_panel.history.clear();
   } else if (command == "help") {
     PrintToConsole("wat a n00b");
-    PrintToConsole("you're on ur own fur now");
+    PrintToConsole("forl real view README.md for instructions")
   } else if (command == "") {
     PrintToConsole("");
   } else if (command == "sh" && player.sh_unlocked) {
